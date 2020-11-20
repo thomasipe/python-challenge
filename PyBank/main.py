f025240af5fb4f08a54e7e940234c7c745ca4eb9
Thomas Sipe <tsipe@Toms-MacBook-Pro.local>
2020-11-20 12:30:39 -0500

    PyBank solved for total months and net pl

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,34 @@
-print("Hello World")
+import os
+import csv
+
+# Path to collect data from the Resources folder
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    #header 
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}") 
+
+    tot_months = 0
+    net_pl = 0
+    pl = []
+    for row in csvreader:
+        # total number of months
+        tot_months = tot_months + 1
+
+        #net amount of Profit/Loss
+        
+        net_pl = net_pl + int(row[1])
+
+        #avg of profit/loss
+        #print(row[1])           
+
+print(tot_months)
+print(net_pl)
+
+
+
+
+
